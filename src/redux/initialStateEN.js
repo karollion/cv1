7d37d1523700d22158fb3c4321299270ad64e6c7
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-11-01 21:30:24 +0100

    Change layout

--- src/redux/initialStateEN.js
+++ src/redux/initialStateEN.js
@@ -117,40 +117,6 @@ const initialState = {
       link: 'https://wdp-2309-01.netlify.app/',
     },
     {
-      id: 3,
-      title: 'CV website',
-      tech: [
-        {
-          id: 1,
-          color: {background: '#f16529'},
-          name: 'HTML',
-        },
-        {
-          id: 2,
-          color: {background: '#29a5d1'},
-          name: 'CSS',
-        },
-        {
-          id: 3,
-          color: {background: '#ffdf00'},
-          name: 'JavaScript',
-        },
-        {
-          id: 4,
-          color: {background: '#00d5ff'},
-          name: 'React',
-        },
-        {
-          id: 5,
-          color: {background: '#764abc'},
-          name: 'Redux',
-        },
-      ],
-      img: 'cvpage',
-      about: 'My home page. It contains a few sentences about me, information about my professional experience and education. There are also projects made by me.',
-      link: 'https://cv1--karollion.repl.co/',
-    },
-    {
       id: 1,
       title: 'Music Service',
       tech: [
