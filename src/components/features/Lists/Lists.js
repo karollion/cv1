67cb2ce2ecba0837a7850395f6d67e05274d26e0
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-11-07 17:35:14 +0100

    remove carusel

--- src/components/features/Lists/Lists.js
+++ src/components/features/Lists/Lists.js
@@ -2,19 +2,30 @@ import styles from './Lists.module.scss';
 import ListElement from '../../common/ListElement/ListElement';
 import { useSelector } from 'react-redux';
 import { getTechnologies, getSkills} from '../../../redux/store';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Lists = props => {
   const skills = useSelector(getSkills);
   const technologies = useSelector(getTechnologies);
+  AOS.init();
 
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
+      <div 
+        data-aos="fade-right" 
+        data-aos-delay="500"
+        data-aos-once="true"
+        className={styles.left}>
         <ul>
           {skills.map(skill => <ListElement key={skill.id} {...skill} />)}
         </ul>
       </div>
-      <div className={styles.right}>
+      <div 
+        data-aos="fade-left" 
+        data-aos-delay="500"
+        data-aos-once="true"
+        className={styles.right}>
         <ul>
         {technologies.map(tech => <ListElement key={tech.id} {...tech} />)}
         </ul>
--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -3,53 +3,21 @@ import { useSelector } from 'react-redux';
 import { getAllProjects } from '../../../redux/store';
 import Project from '../../features/Project/Project';
 import ContainerSection from '../../common/ContainerSection/ContainerSection';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Projects = () => {
   const projects = useSelector(getAllProjects);
-
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
+  AOS.init();
 
   return (
     <ContainerSection>
-        <div className={styles.projects}>
-        <Carousel
-          swipeable={true}
-          draggable={false}
-          showDots={true}
-          responsive={responsive}
-          ssr={true} // means to render carousel on server-side.
-          infinite={false}
-          autoPlay={false}
-          autoPlaySpeed={1000}
-          keyBoardControl={true}
-          customTransition="all .5"
-          transitionDuration={500}
-          containerClass="carousel-container"
-          removeArrowOnDeviceType={["tablet", "mobile"]}
-          //deviceType={this.props.deviceType}
-          dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-40-px"
-        >
+        <div 
+          data-aos="fade-right" 
+          data-aos-delay="500"
+          data-aos-once="true"
+          className={styles.projects}>
           {projects.map(project => <Project key={project.id} {...project}  />)}
-        </Carousel>
       </div>
     </ContainerSection>
   );
